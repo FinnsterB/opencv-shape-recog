47e848c7404d1e0b0f8cbb3234627ee4ce415b8a
FinnsterB <finnybrueren@gmail.com>
2024-02-25 20:41:52 +0100

    Removed example code for shape tracker

--- Main.cpp
+++ Main.cpp
@@ -9,33 +9,6 @@ cv::Mat originalImage;
 cv::Mat contourOutput;
 cv::Mat contourImage;
 
-void thresholdOnChange(int aThreshold, void* userData){
-	
-	cv::imshow("Threshold Image", image);
-	cv::imshow("Output Image", contourImage);
-
-	threshold = aThreshold;
-
-	//Prepare the image for findContours
-	cv::blur(image, image, cv::Size(5,5), cv::Point(2,3), cv::BORDER_REFLECT);
-    cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
-    cv::threshold(image, image, threshold, 255, 1);
-
-    //Find the contours. Use the contourOutput Mat so the original image doesn't get overwritten
-    std::vector<std::vector<cv::Point> > contours;
-    contourOutput = image.clone();
-    cv::findContours( contourOutput, contours, cv::RETR_LIST, cv::CHAIN_APPROX_NONE );
-
-    //Draw the contours
-    cv::Scalar colors[3];
-    colors[0] = cv::Scalar(255, 0, 0);
-    colors[1] = cv::Scalar(0, 255, 0);
-    colors[2] = cv::Scalar(0, 0, 255);
-    for (size_t idx = 0; idx < contours.size(); idx++) {
-        cv::drawContours(contourImage, contours, idx, colors[idx % 3]);
-    }
-}
-
 int main(int argc, const char * argv[]) {
 
     image= cv::imread("../sample.jpeg");
@@ -44,31 +17,7 @@ int main(int argc, const char * argv[]) {
         std::cout << "Image file not found\n";
         return 1;
     }
-
-	cv::Mat originalImage = image.clone();
-
-    //Prepare the image for findContours
-	cv::blur(image, image, cv::Size(5,5), cv::Point(2,3), cv::BORDER_REFLECT);
-    cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
-    cv::threshold(image, image, 25, 255, 1);
-
-    //Find the contours. Use the contourOutput Mat so the original image doesn't get overwritten
-    std::vector<std::vector<cv::Point> > contours;
-    contourOutput = image.clone();
-    cv::findContours( contourOutput, contours, cv::RETR_LIST, cv::CHAIN_APPROX_NONE );
-
-    //Draw the contours
-    contourImage = cv::Mat(image.size(), CV_8UC3, cv::Scalar(0,0,0));
-    cv::Scalar colors[3];
-    colors[0] = cv::Scalar(255, 0, 0);
-    colors[1] = cv::Scalar(0, 255, 0);
-    colors[2] = cv::Scalar(0, 0, 255);
-    for (size_t idx = 0; idx < contours.size(); idx++) {
-        cv::drawContours(contourImage, contours, idx, colors[idx % 3]);
-    }
-	std::cout << contours.size() << " shapes found!!!" << std::endl;
-    cv::imshow("Input Image", originalImage);
-	cv::createTrackbar("Threshold", "Input Image", nullptr, 100, thresholdOnChange);
+    
 
 	cv::imshow("Threshold Image", image);
 	cv::imshow("Output Image", contourImage);
